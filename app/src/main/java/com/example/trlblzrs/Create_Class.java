5ae4598574031fb4494745514210177089e3d270
Syed Ali Jibran Rizvi <syedalijibranrizvi@gmail.com>
2022-08-25 17:31:37 +0530

    Changes MAde to activity_create_class.xml and Create_Class.java

--- app/src/main/java/com/example/trlblzrs/Create_Class.java
+++ app/src/main/java/com/example/trlblzrs/Create_Class.java
@@ -1,14 +1,54 @@
 package com.example.trlblzrs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Create_Class extends AppCompatActivity {
 
+    List<String> MyList;
+    Button b;
+    TextInputEditText clas,sub;
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        b=findViewById(R.id.button2);
+        clas= (TextInputEditText) findViewById(R.id.cla);
+        sub=findViewById(R.id.sub);
+        reference= FirebaseDatabase.getInstance().getReference();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_class);
+        MyList=new ArrayList<>();
+        b.setOnClickListener(v -> {
+            uploadList(clas.getText().toString(),sub.getText().toString());
+        });
+    }
+
+    public void uploadList(String s1, String s2) {
+        MyList.add(s1+" "+s2);
+        reference.setValue(MyList).addOnCompleteListener(task -> {
+            if(task.isSuccessful())
+            {
+                Toast.makeText(getApplicationContext(),"Class has been created",Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                    Toast.makeText(getApplicationContext(),"Retry After Some Time!",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
