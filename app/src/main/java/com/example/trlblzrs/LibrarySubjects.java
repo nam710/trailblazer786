135c14b72d6ee5781feea4113ac00021a50aa8e0
nam710 <namanchawla029@gmail.com>
2022-08-25 12:30:47 +0530

    minor changes

---
+++ app/src/main/java/com/example/trlblzrs/LibrarySubjects.java
@@ -0,0 +1,14 @@
+package com.example.trlblzrs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LibrarySubjects extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library_subjects);
+    }
+}
--- app/src/main/java/com/example/trlblzrs/MyLibrary.java
+++ app/src/main/java/com/example/trlblzrs/MyLibrary.java
@@ -2,7 +2,9 @@ package com.example.trlblzrs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyLibrary extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MyLibrary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_library);
+    }
+
+    public void openActivity(View v){
+        Intent i =new Intent(MyLibrary.this,LibrarySubjects.class);
+        startActivity(i);
     }
 }
